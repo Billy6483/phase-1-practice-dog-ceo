db6e98027d696a365abc2724c4e6966df8c240bf
Billy <koechkbilly@gmail.com>
2024-07-13 05:23:36 +0300

    commit all

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 console.log('%c HI', 'color: firebrick')
 
-
 //const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 document.addEventListener('DOMContentLoaded',function(){
     fetch("https://dog.ceo/api/breeds/image/random/4")
